63a54aaadd75b9220d50f0341e30342cf1e7ed80
CcydtN <51289140+CcydtN@users.noreply.github.com>
2024-01-17 00:06:35 -0500

    PointerOverflow crypto

---
+++ 2023/PointerOverflow/crypto/a_pale_violet_light/solve.py
@@ -0,0 +1,24 @@
+from Crypto.Util.number import long_to_bytes
+import subprocess
+import os
+
+e = 5039
+N = 34034827
+C = [933969,15848125,24252056,5387227,5511551,10881790,3267174,14500698,28242580,933969,32093017,18035208,2594090,2594090,9122397,21290815,15930721,4502231,5173234,21290815,23241728,2594090,21290815,18035208,10891227,15930721,202434,202434,21290815,5511551,202434,4502231,5173234,25243036]
+# C = long_to_bytes(C)
+C = map(str,C)
+C = ",".join(C)
+
+temp_file = f"{os.getcwd()}/tmp.txt"
+if os.path.exists(temp_file):
+    os.remove(temp_file)
+
+command = ["rsactftool", "--decrypt", C, "-e", f"{e}", "-n", f"{N}", "--output", temp_file]
+subprocess.run(command)
+
+with open("./tmp.txt", "rb") as file:
+    content=file.read()
+    print(content.decode().replace(" ", "_"))
+
+os.remove(temp_file)
+  
---
+++ 2023/PointerOverflow/crypto/unquestioned_and_unrestrained/readme.md
@@ -0,0 +1,6 @@
+# Problem
+Given a encoded string `cG9jdGZ7dXdzcF80MTFfeTB1Ml84NDUzXzQyM184MzEwbjlfNzBfdTV9`, try decode.
+
+# Solution
+Decoded with base64, and the source is ASCII
+`poctf{uwsp_411_y0u2_8453_423_8310n9_70_u5}`
